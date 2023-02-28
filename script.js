2ded1451ea544f9b3c9ae3d12ce5da4fb3e5b2b3
Pame86 <125613369+Pame86@users.noreply.github.com>
2023-02-27 22:34:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function mostrarTelefono() {
+    if (document.getElementById("phone").className == "visible")
+        document.getElementById("phone").className = "oculto";
+    else 
+        document.getElementById("phone").className = "visible";
+}
+
+function mostrarMail() {
+    if (document.getElementById("mail").className == "visible")
+        document.getElementById("mail").className = "oculto";
+    else 
+        document.getElementById("mail").className = "visible";
+}
+
+function mostrarDireccion() {
+    if (document.getElementById("direccion").className == "visible")
+        document.getElementById("direccion").className = "oculto";
+    else 
+        document.getElementById("direccion").className = "visible";
+}
+
